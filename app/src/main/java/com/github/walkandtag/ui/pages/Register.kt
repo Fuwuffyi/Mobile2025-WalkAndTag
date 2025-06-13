5ef25de5b85feab920f11216303602822693677a
Fuwuffyi <luca.palazzini3@studio.unibo.it>
2025-06-13 14:45:32 +0200

    Improve UI for login and register screens

--- app/src/main/java/com/github/walkandtag/ui/pages/Register.kt
+++ app/src/main/java/com/github/walkandtag/ui/pages/Register.kt
@@ -1,41 +1,114 @@
 package com.github.walkandtag.ui.pages
 
 import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 
 @Composable
 fun Register(navController: NavController) {
-    Column(
+    var email: String by remember { mutableStateOf("") }
+    var password: String by remember { mutableStateOf("") }
+    var repeat_password: String by remember { mutableStateOf("") }
+
+    Surface(
         modifier = Modifier
             .fillMaxSize()
-            .padding(24.dp)
+            .background(MaterialTheme.colorScheme.background)
     ) {
-        Text("Register")
-        Row {
-            Button(
-                modifier = Modifier.weight(1.0f),
-                onClick = { Log.i("Register", "Register: Register pressed") }
-            ) {
-                Text("Register")
-            }
-            Spacer(modifier = Modifier.width(6.dp))
-            OutlinedButton(
-                modifier = Modifier.weight(1.0f),
-                onClick = { navController.navigate("login") }
-            ) {
-                Text("Login")
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(24.dp)
+                .wrapContentSize(Alignment.Center)
+        ) {
+            Text("Register",
+                style = MaterialTheme.typography.headlineMedium.copy(fontSize = 30.sp),
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(bottom = 32.dp)
+            )
+            OutlinedTextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text("Email") },
+                singleLine = true,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp),
+                shape = RoundedCornerShape(8.dp)
+            )
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Password") },
+                singleLine = true,
+                visualTransformation = PasswordVisualTransformation(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 24.dp),
+                shape = RoundedCornerShape(8.dp)
+            )
+            OutlinedTextField(
+                value = repeat_password,
+                onValueChange = { repeat_password = it },
+                label = { Text("Repeat Password") },
+                singleLine = true,
+                visualTransformation = PasswordVisualTransformation(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 24.dp),
+                shape = RoundedCornerShape(8.dp)
+            )
+            Row {
+                Button(
+                    onClick = { Log.i("Register", "Register: Register pressed") },
+                    shape = RoundedCornerShape(8.dp),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(50.dp)
+                        .weight(1.0f)
+                ) {
+                    Text("Register")
+                }
+                Spacer(modifier = Modifier.width(6.dp))
+                OutlinedButton(
+                    onClick = { navController.navigate("login") },
+                    shape = RoundedCornerShape(8.dp),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(50.dp)
+                        .weight(1.0f)
+                ) {
+                    Text("Login")
+                }
             }
         }
     }
