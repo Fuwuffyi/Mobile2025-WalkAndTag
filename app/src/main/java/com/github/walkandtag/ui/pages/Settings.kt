861b3f1c59599f02a3e89eb9dd563513e7f0e752
Luca Ponseggi <luca.ponseggi@studio.unibo.it>
2025-06-18 17:41:54 +0200

    Change some color in Settings page

--- app/src/main/java/com/github/walkandtag/ui/pages/Settings.kt
+++ app/src/main/java/com/github/walkandtag/ui/pages/Settings.kt
@@ -2,12 +2,14 @@ package com.github.walkandtag.ui.pages
 
 import android.app.Activity
 import android.content.Intent
+import androidx.compose.foundation.gestures.snapping.SnapPosition
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -19,9 +21,9 @@ import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.LightMode
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -29,8 +31,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.github.walkandtag.AuthActivity
 import com.github.walkandtag.firebase.auth.Authentication
+import com.github.walkandtag.ui.theme.md_theme_dark_primary
+import com.github.walkandtag.ui.theme.md_theme_light_primary
 import org.koin.compose.koinInject
 
 @Composable
@@ -44,13 +49,16 @@ fun Settings() {
             .padding(24.dp)
             .wrapContentSize(Alignment.Center)
     ) {
-        Text("Settings")
         Row(
             modifier = Modifier
                 .fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Text("Cambia tema", modifier = Modifier.weight(1f))
+            Text(
+                "Cambia tema",
+                modifier = Modifier.weight(1f),
+                style = MaterialTheme.typography.headlineMedium.copy(fontSize = 30.sp)
+            )
             Row(
                 modifier = Modifier
                     .width(100.dp),
@@ -60,24 +68,30 @@ fun Settings() {
                     Icon(
                         imageVector = Icons.Default.LightMode,
                         contentDescription = "Imposta modalità chiara",
-                        tint = Color.Yellow
+                        tint = MaterialTheme.colorScheme.primary,
+                        modifier = Modifier.size(70.dp)
                     )
                 }
                 IconButton(onClick = { /* Azione */ }) {
                     Icon(
                         imageVector = Icons.Default.DarkMode,
                         contentDescription = "Imposta modalità scura",
-                        tint = Color.Yellow
+                        tint = MaterialTheme.colorScheme.secondary,
+                        modifier = Modifier.size(70.dp)
                     )
                 }
             }
         }
+        Spacer(modifier = Modifier.height(50.dp))
         Row(
             modifier = Modifier
                 .fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Text("Modifica profilo", modifier = Modifier.weight(1f))
+            Text(
+                "Modifica profilo",
+                modifier = Modifier.weight(1f),
+                style = MaterialTheme.typography.headlineMedium.copy(fontSize = 30.sp))
             Box(
                 modifier = Modifier
                     .width(100.dp)
@@ -89,17 +103,22 @@ fun Settings() {
                     Icon(
                         imageVector = Icons.Default.Edit,
                         contentDescription = "Modifica account",
-                        tint = Color.Yellow
+                        tint = MaterialTheme.colorScheme.primary,
+                        modifier = Modifier.size(70.dp)
                     )
                 }
             }
         }
+        Spacer(modifier = Modifier.height(50.dp))
         Row(
             modifier = Modifier
                 .fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Text("Elimina account", modifier = Modifier.weight(1f))
+            Text(
+                "Elimina account",
+                modifier = Modifier.weight(1f),
+                style = MaterialTheme.typography.headlineMedium.copy(fontSize = 30.sp))
             Box(
                 modifier = Modifier
                     .width(100.dp)
@@ -111,25 +130,31 @@ fun Settings() {
                     Icon(
                         imageVector = Icons.Default.Delete,
                         contentDescription = "Elimina account",
-                        tint = Color.Red
+                        tint = MaterialTheme.colorScheme.error,
+                        modifier = Modifier.size(70.dp)
                     )
                 }
             }
         }
+        Spacer(modifier = Modifier.height(150.dp))
         Button(
             onClick = {
                 authentication.logout()
                 val intent = Intent(context, AuthActivity::class.java)
                 context.startActivity(intent)
                 (context as? Activity)?.finish()
-            }
+            },
+            modifier = Modifier.align(Alignment.CenterHorizontally)
         ) {
-            Text("Logout")
+            Text(
+                "Logout",
+                style = MaterialTheme.typography.headlineMedium.copy(fontSize = 30.sp)
+            )
             Spacer(modifier = Modifier.width(8.dp))
             Icon(
                 imageVector = Icons.AutoMirrored.Filled.Logout,
                 contentDescription = "Logout",
-                modifier = Modifier.size(20.dp)
+                modifier = Modifier.size(30.dp)
             )
         }
     }
