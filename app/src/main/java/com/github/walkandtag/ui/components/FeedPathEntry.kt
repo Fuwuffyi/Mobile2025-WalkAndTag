f4e005256ea3d14b16077a75bf5e445c477ddb07
Luca Ponseggi <luca.ponseggi@studio.unibo.it>
2025-06-19 17:36:35 +0200

    Small fixes

--- app/src/main/java/com/github/walkandtag/ui/components/FeedPathEntry.kt
+++ app/src/main/java/com/github/walkandtag/ui/components/FeedPathEntry.kt
@@ -1,18 +1,28 @@
 package com.github.walkandtag.ui.components
 
+import android.util.Log
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ShowChart
 import androidx.compose.material.icons.filled.PinDrop
+import androidx.compose.material.icons.filled.StarBorder
 import androidx.compose.material.icons.filled.SupervisedUserCircle
+import androidx.compose.material.icons.filled.Timer
+import androidx.compose.material.icons.outlined.Star
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -20,19 +30,56 @@ import java.util.Locale
 
 @Composable
 fun FeedPathEntry(username: String, length: Float, duration: Float) {
+    Spacer(modifier = Modifier.size(40.dp))
     Column {
-        Row {
+        Row(modifier = Modifier.padding(6.dp), verticalAlignment = Alignment.CenterVertically) {
             Icon(Icons.Filled.SupervisedUserCircle, "Profile Icon")
-            Text(username)
+            Text(username, modifier = Modifier.padding(start = 4.dp))
         }
         Row {
-            Box(modifier = Modifier.fillMaxWidth().height(256.dp).background(Color(150, 255, 200)))
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(256.dp)
+                    .background(Color(150, 255, 200))
+            ) {
+                IconButton(
+                    onClick = { Log.i("TEST", "FeedPathEntry: ") },
+                    modifier = Modifier
+                        .align(Alignment.TopStart)
+                        .padding(8.dp) // margine dalla cornice
+                        .border(2.dp, Color.Yellow)
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.StarBorder,
+                        contentDescription = "Favorite",
+                        tint = Color.Yellow,
+                        modifier = Modifier.size(50.dp)
+                    )
+                }
+            }
         }
-        Row {
-            Text("${String.format(Locale.ITALY ,"%.2f", length)}km")
-            Icon(Icons.Filled.PinDrop, "Length")
-            Icon(Icons.AutoMirrored.Filled.ShowChart, "Duration (h)")
-            Text("${String.format(Locale.ITALY ,"%.2f", duration)}h")
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(6.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Text(
+                    text = "${String.format(Locale.ITALY, "%.2f", length)}km",
+                    modifier = Modifier.padding(end = 4.dp)
+                )
+                Icon(Icons.Filled.PinDrop, contentDescription = "Length")
+            }
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Icon(Icons.Filled.Timer, contentDescription = "Duration (h)")
+                Text(
+                    text = "${String.format(Locale.ITALY, "%.2f", duration)}h",
+                    modifier = Modifier.padding(start = 4.dp)
+                )
+            }
         }
     }
 }
--- app/src/main/java/com/github/walkandtag/ui/pages/Home.kt
+++ app/src/main/java/com/github/walkandtag/ui/pages/Home.kt
@@ -1,7 +1,11 @@
 package com.github.walkandtag.ui.pages
 
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.github.walkandtag.ui.components.FeedPathEntry
 
 @Composable
