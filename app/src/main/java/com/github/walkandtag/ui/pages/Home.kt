1730a196fc648e71fc7173c33dab7ae6c6628bad
Luca Ponseggi <luca.ponseggi@studio.unibo.it>
2025-06-28 15:12:14 +0200

    Code clean

--- app/src/main/java/com/github/walkandtag/ui/pages/Home.kt
+++ app/src/main/java/com/github/walkandtag/ui/pages/Home.kt
@@ -1,13 +1,11 @@
 package com.github.walkandtag.ui.pages
 
-import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
