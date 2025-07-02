97b2abad640eb1032dc5e131b371800df420ff66
Fuwuffyi <luca.palazzini3@studio.unibo.it>
2025-07-02 09:40:31 +0200

    Clean up imports

--- app/src/main/java/com/github/walkandtag/ui/pages/Profile.kt
+++ app/src/main/java/com/github/walkandtag/ui/pages/Profile.kt
@@ -1,6 +1,5 @@
 package com.github.walkandtag.ui.pages
 
-import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
