f7083b4c31e3108a309f5a305bd13afa981fd3e8
Fuwuffyi <luca.palazzini3@studio.unibo.it>
2025-07-02 11:11:57 +0200

    Add path recording service

--- app/src/main/java/com/github/walkandtag/service/PathRecordingService.kt
+++ app/src/main/java/com/github/walkandtag/service/PathRecordingService.kt
@@ -1,2 +1,81 @@
 package com.github.walkandtag.service
 
+import android.Manifest
+import android.app.Service
+import android.content.Intent
+import android.location.Location
+import android.os.IBinder
+import androidx.annotation.RequiresPermission
+import androidx.core.app.NotificationCompat
+import com.github.walkandtag.util.Notifier
+import com.google.android.gms.location.*
+import kotlinx.coroutines.*
+import org.koin.android.ext.android.inject
+import org.maplibre.android.geometry.LatLng
+
+class PathRecordingService : Service() {
+    private val pathRecordingNotificationId = 420
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationCallback: LocationCallback
+    private val coroutineScope = CoroutineScope(Dispatchers.IO + SupervisorJob())
+    private val pathPoints = mutableListOf<LatLng>()
+    private val notifier: Notifier by inject()
+
+    @RequiresPermission(Manifest.permission.POST_NOTIFICATIONS)
+    override fun onCreate() {
+        super.onCreate()
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        val notification = notifier.notify(
+            title = "Recording Path",
+            text = "Your walk is being recorded.",
+            notificationId = pathRecordingNotificationId,
+            ongoing = true,
+            NotificationCompat.PRIORITY_LOW
+        )
+        startForeground(pathRecordingNotificationId, notification)
+    }
+
+    @RequiresPermission(anyOf = [Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION])
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        startLocationUpdates()
+        return START_STICKY
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        stopLocationUpdates()
+        notifier.cancel(pathRecordingNotificationId)
+        coroutineScope.cancel()
+        // @TODO(): Persist each point for crash safety
+    }
+
+    override fun onBind(intent: Intent?): IBinder? = null
+
+    @RequiresPermission(anyOf = [Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION])
+    private fun startLocationUpdates() {
+        val locationRequest = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 3000L)
+            .setMinUpdateDistanceMeters(10f).setMinUpdateIntervalMillis(1000L).build()
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(result: LocationResult) {
+                for (location in result.locations) {
+                    addPoint(location)
+                }
+            }
+        }
+        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, mainLooper)
+    }
+
+    private fun stopLocationUpdates() {
+        if (::locationCallback.isInitialized) {
+            fusedLocationClient.removeLocationUpdates(locationCallback)
+        }
+    }
+
+    private fun addPoint(location: Location) {
+        val latLng = LatLng(location.latitude, location.longitude)
+        pathPoints.add(latLng)
+        // @TODO(): Persist each point for crash safety
+    }
+
+    fun getPath(): Collection<LatLng> = pathPoints
+}
--- app/src/main/java/com/github/walkandtag/ui/pages/Profile.kt
+++ app/src/main/java/com/github/walkandtag/ui/pages/Profile.kt
@@ -1,5 +1,6 @@
 package com.github.walkandtag.ui.pages
 
+import android.content.Intent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -18,6 +19,8 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import com.github.walkandtag.service.PathRecordingService
 import com.github.walkandtag.ui.components.FeedPathEntry
 import com.github.walkandtag.ui.navigation.Navigation
 import com.github.walkandtag.ui.viewmodel.GlobalViewModel
@@ -38,9 +41,10 @@ fun Profile(
         viewModel.loadUserProfile(userId)
     }
 
+    val context = LocalContext.current
     val state = viewModel.uiState.collectAsState()
 
-    // @TODO(): Unsure if I should clean this up
+    // @TODO(): Unsure if I should clean this up or move to viewModel
     val locationPermissionHandler = rememberMultiplePermissions(
         permissions = listOf(
             android.Manifest.permission.ACCESS_COARSE_LOCATION,
@@ -48,6 +52,11 @@ fun Profile(
         )
     ) { status ->
         if (status.values.any { it.isGranted }) {
+            if (!state.value.isRecording) {
+                context.startForegroundService(Intent(context, PathRecordingService::class.java))
+            } else {
+                context.stopService(Intent(context, PathRecordingService::class.java))
+            }
             viewModel.toggleRecording()
         } else {
             globalViewModel.showSnackbar("You do not have location permissions")
@@ -71,7 +80,9 @@ fun Profile(
             Row {
                 if (viewModel.isOwnProfile()) {
                     ElevatedButton(
-                        onClick = { locationPermissionHandler.launchPermissionRequest() }) {
+                        onClick = {
+                            locationPermissionHandler.launchPermissionRequest()
+                        }) {
                         Text(if (state.value.isRecording) "Save Path" else "Record Path")
                     }
                 }
