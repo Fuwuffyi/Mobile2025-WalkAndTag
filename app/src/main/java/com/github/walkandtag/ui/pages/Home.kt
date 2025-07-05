4fa5dbf9dccd30c9ada73ea6a7dc5080938b0b20
Fuwuffyi <luca.palazzini3@studio.unibo.it>
2025-07-05 11:11:06 +0200

    Clean up imports

--- app/src/main/java/com/github/walkandtag/ui/pages/Home.kt
+++ app/src/main/java/com/github/walkandtag/ui/pages/Home.kt
@@ -35,7 +35,6 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.github.walkandtag.R
